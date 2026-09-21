1896ee9cc620a907b8f9a49597b125fe2bb110ef
gusttavodeodato <guustavodeodato@outlook.com>
2026-09-21 20:19:34 -0300

    feat: adiciona endpoint de health check

---
+++ src/main/java/dev/deodato/tcc/petshop/controller/HealthController.java
@@ -0,0 +1,12 @@
+package dev.deodato.tcc.petshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public String health() {
+        return "OK";
+    }
+}
